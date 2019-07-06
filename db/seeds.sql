52f90347015e117a24d1bca5a895277e5218e685
Bee <be04zimm@gmail.com>
2019-07-05 23:37:24 -0400

    updatine db folder with schema and seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,9 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+-- Seed Burgers into Database 
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Big & Beefy", false),
+    ("Plain & Simple", false),
+    ("Cheezetastic", false),
+    ("Blue Bacon", false);
