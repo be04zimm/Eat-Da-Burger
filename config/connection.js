9b8892c5cc0572115204cdd3f15ec3381b8b4376
Bee <be04zimm@gmail.com>
2019-07-10 15:18:19 -0400

    working app

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,12 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "Sky23high",
   database: "burgers_db"
 });
 
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
